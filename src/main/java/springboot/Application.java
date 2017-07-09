1fe92627dbc20d4a7262fc44e4e7ebbd29f78331
Ilyaros <ilyaros@yandex.ru>
2017-07-10 00:09:14 +0300

    10.07.2017

--- src/main/java/springboot/Application.java
+++ src/main/java/springboot/Application.java
@@ -5,21 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * Created by Ilyaros on 06.07.2017.
- */
 
-@Controller
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class);
     }
-
-
-    @RequestMapping("/hello")
-    public String showHelloWorld(){
-        return "hello-world";
-    }
-
 }
